d447d0b14bb102609185e378d8bb471f67ae8f5c
Kirill Chirkov <kirill.chirkov1997@mail.ru>
2024-05-26 14:49:47 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/Chirkov402/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,21 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const container = document.querySelector('.places__list');
+
+function addCards () {
+    const cardTemplate = document.querySelector('#card-template').content;
+    initialCards.forEach((item) => {
+        let cardElements = cardTemplate.querySelector('.places__item').cloneNode(true);
+        cardElements.querySelector('.card__image').src = item.link;
+        cardElements.querySelector('.card__title').textContent = item.name;
+        cardElements.querySelector('.card__delete-button').addEventListener('click', function (evt) {
+            let cardDelete = evt.target.closest('.card');
+            cardDelete.remove();
+        })
+        container.appendChild(cardElements);
+    })
+}
+
+addCards();
